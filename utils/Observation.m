422b260c821aa21a6112ded4b1e5152b73739b21
SangliTeng <37236559+SangliTeng@users.noreply.github.com>
2021-03-31 22:04:54 +0800

    Leg order fixed in obs

--- utils/Observation.m
+++ utils/Observation.m
@@ -1,9 +1,9 @@
 function v = Observation(q,dq,contact)
     v = [];
     if contact(1) > 0.5
-        v = [v;-J_RightToeBottom(q) * dq];
+        v = [v;-J_LeftToeBottom(q) * dq];
     end
     if contact(2) > 0.5
-        v = [v;-J_LeftToeBottom(q) * dq];
+        v = [v;-J_RightToeBottom(q) * dq];
     end
 end
