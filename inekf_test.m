0a241d6427094e59ce0c9a195de96f29f40e6165
potBagMeat <369230018@qq.com>
2021-04-13 22:40:26 +0800

    tune some paramters and fix some minor mistakes

--- inekf_test.m
+++ inekf_test.m
@@ -1,5 +1,5 @@
-clear;close;clc;
-proceed_data;
+ clear;close;clc;
+ proceed_data;
 dt = diff(Time);
 dt = [dt;dt(1)];
 %% Data
@@ -18,11 +18,11 @@ b_g = zeros(3,1);
 g = [0 0 -9.8067]'; % gravitational force vector
 noise_omega =  [1,1,1] * 1e-4;
 noise_acc = [1,1,1] * 1e-4;
-noise_ba = [1,1,1] * 1e-12;
-noise_bg = [1,1,1] * 1e-12;
+noise_ba = [1,1,1] * 1e-5;
+noise_bg = [1,1,1] * 1e-5;
 Cov_noise = blkdiag(diag(noise_omega), diag(noise_acc), diag(zeros(1,3)), diag(noise_ba), diag(noise_bg));
 %% InEKF loop
-for k = 1:length(IMU)
+for k = 2000 * 3:length(IMU)
     %% propagation
     % step 1: X = f(X,u)
     cur_acc = IMU(k,1:3)';                                                  % extract current acceleration from data , frame: robot frame
@@ -30,7 +30,7 @@ for k = 1:length(IMU)
 %     R_t = eul2rotm(q_SE3(k, 4:6), 'ZYX');
 %     cur_acc = R_t \ IMU(k, [1:3])';
 %     cur_omega = R_t \ IMU(k, [4:6])';
-    R = R * expm(skew(cur_omega - b_g)*dt(k));                              % update R matrix
+    R = R * expm(skew(cur_omega - b_g)*dt(k));                             % update R matrix
     v = v + R * (cur_acc - b_a) * dt(k) + g * dt(k);                       % update v vector
     p = p + v * dt(k) + 1/2 * (R * ( cur_acc - b_a ) + g) * dt(k) * dt(k); % update p
     %b_a = b_a;                                              
@@ -50,7 +50,8 @@ for k = 1:length(IMU)
     A(4:6, 10:12) = -skew(v)*R;
     A(7:9,10:12) = -skew(p)*R;
     A(4:6, 13:15) = - R;
-    %A(10:15, 10:15) = eye(6);
+    % A(10:15, 10:15) = eye(6);
+    
     % compute adjoint matrix of xi
     xi_adj = zeros(9,9);
     xi_adj(1:3,1:3) = R;
@@ -71,7 +72,7 @@ for k = 1:length(IMU)
     %% correction
     % form the kinematics measurementes. 
     H = [];
-    z = [];
+    % z = [];
     if contact(k,1) > 0.99 % left is in contact
         H = [zeros(3), -eye(3), zeros(3,9)];        
         vb = Jp_VectorNav_to_LeftToeBottom(q_leg(k,:)) * dq_leg(k,:)';
@@ -79,10 +80,8 @@ for k = 1:length(IMU)
         v = cross(IMU(k,4:6), pb)' + vb;
         [xi, P, b_a, b_g] = Observation_RIEKF(xi,P,b_a,b_g,v);
         % z = [z;v];
-
     end
     if contact(k,2) > 0.99 % right is in contact
-
         H = [zeros(3), -eye(3), zeros(3,9)];       
         vb = Jp_VectorNav_to_RightToeBottom(q_leg(k,:)) * dq_leg(k,:)';
         pb = p_VectorNav_to_RightToeBottom(q_leg(k,:));
@@ -90,24 +89,16 @@ for k = 1:length(IMU)
         [xi, P, b_a, b_g] = Observation_RIEKF(xi,P,b_a,b_g,v);
         % z = [z;v];
     end
-    %excecute the covariance update steps
-   if isempty(H)
-%         eul_angle = rotm2eul(xi(1:3,1:3),'ZYX');
-%         position = xi(1:3,5);
-%         vel = xi(1:3,4);
-%         q_SE3_(k,1:6) = [position' eul_angle];
-%     
-%         dq_SE3_(k,1:6) = [vel' cur_omega'];
-   end
-    eul_angle = rotm2eul(xi(1:3,1:3),'ZYX');
-    position = xi(1:3,5);
-    vel = xi(1:3,4);
-    q_SE3_(k,1:6) = [-position' eul_angle];
+    % excecute the covariance update steps
     
-    dq_SE3_(k,1:6) = [-vel' cur_omega'];
+    R = xi(1:3,1:3);
+    p = xi(1:3,5);
+    v = xi(1:3,4);
     
+    q_SE3_(k,1:6) = [p' rotm2eul(R)];
+    dq_SE3_(k,1:6) = [v' cur_omega'];
 end
-
+%%
 close all
 figure
 seq = [1,3,5,2,4,6];
@@ -117,7 +108,7 @@ for k = 1:6
     plot(Time(1:end),dq_SE3_(1:length(Time),k),'b')
     plot(Time(1:end),dq_SE3_b_ref(1:length(Time),k),'r-.')
     xlim([0,Time(end)])
-    ylim([-1,1])
+    % ylim([-1,1])
 end
 
 figure
@@ -130,9 +121,42 @@ for k = 1:6
         % plot(Time(1:N:end),pos_int(dq_SE3_(1:N:length(Time),k),dt),'g')
     else
         plot(Time(1:end),wrapTo2Pi(q_SE3_(1:length(Time),k) + pi) - pi,'b')
-        %plot(Time(1:end),q_SE3_(1:length(Time),k) - pi ,'b')
     end
     plot(Time(1:end),q_SE3(1:length(Time),k),'r-.')
     xlim([0,Time(end)])
     % ylim([-2,2])
+end
+%%
+function [xi, P, b_a, b_g] =  Observation_RIEKF(xi_,P_,b_a_,b_g_,v)
+% 
+        H = [zeros(3), -eye(3), zeros(3,9)];
+       
+        y = [-v;-1;0];                                                     % measurement
+        b = zeros(5,1);    
+        b(4) = -1;                                                         % construct b vector
+        Cov_nf = blkdiag(1,1,1)*1e-4;                                           % n_f
+        R_ = xi_(1:3,1:3);
+        Nbar = R_ * Cov_nf * R_';                      
+        S = H * P_ * H' + Nbar;
+        K = P_ * H' * inv(S);                                              % filter gain 
+        nu = xi_ * y - b;                                                  % innovation term
+        select_mat = zeros(3,5);                                           % construct selection matrix
+        select_mat(1:3,1:3) = eye(3);
+        %% get correction term for update
+        correction = K * select_mat * nu;
+        correct_imu = correction(1:9);
+        correct_ba = correction(13:15);
+        correct_bg = correction(10:12);
+        correct_mat = zeros(5,5);
+        correct_mat(1:3,1:3) = skew(correct_imu(1:3));
+        correct_mat(1:3,4) = correct_imu(4:6);
+        correct_mat(1:3,5) = correct_imu(7:9);
+%         correct_mat(4,4) = 1;
+%         correct_mat(5,5) = 1;
+        
+        %% update state , covariance and biases
+        xi = expm(correct_mat)*xi_;
+        b_a = b_a_ + correct_ba;
+        b_g = b_g_ + correct_bg;
+        P = (eye(size(P_)) - K*H) * P_ * (eye(size(P_)) - K*H)' + K * Nbar * K'  ;
 end
